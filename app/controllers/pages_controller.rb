c2133dc8cfd6e93495435801cc9e2db9f85cda0c
Felipe Santos <santos.felipe13@gmail.com>
2018-11-05 19:31:28 -0200

    added basic routes, controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
+    if user_signed_in?
+      @user = User.find(current_user.id)
+    end
+
+
+
+
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  get 'ships/show'
-  get 'ships/edit'
-  get 'ships/update'
-  get 'parts/index'
-  get 'parts/show'
-  get 'parts/new'
-  get 'parts/create'
-  get 'parts/edit'
-  get 'parts/update'
-  get 'parts/transaction'
-  get 'parts/destroy'
   devise_for :users
+
+  resources :parts
+  # get 'parts/transaction'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
