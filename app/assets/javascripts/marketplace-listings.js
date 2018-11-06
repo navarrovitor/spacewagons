8bf9bbb08108b0e047b1ace438ede9e6d1f2d312
Felipe Santos <santos.felipe13@gmail.com>
2018-11-06 01:33:33 -0200

    marketplace listings and basic user page skeleton

---
+++ app/assets/javascripts/marketplace-listings.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+// Log shortcut
+function l(x) {
+  console.log(x);
+}
+
+// Find index of item
+function index(el) {
+    var children = el.parentNode.childNodes,
+        i = 0;
+    for (; i < children.length; i++) {
+        if (children[i] == el) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Element listing
+const allListingDetails = document.querySelectorAll(".listing-body");
+const allListingHeaders = document.querySelectorAll(".listing-header");
+const listingCount = allListingHeaders.length
+
+var detailsShown = new Array(100);
+for (var i = 0; i < detailsShown.length; ++i) { detailsShown[i] = false; }
+
+
+l(listingCount)
+// Initializations
+Array.prototype.forEach.call(allListingDetails, a => {
+  a.style.display = "none";
+});
+
+// Listing on click
+
+Array.prototype.forEach.call(allListingHeaders, a => {
+  a.addEventListener("click", function() {
+    indexClick = (index(a.parentNode) - 1)/2
+
+    l(detailsShown[indexClick]);
+    if (detailsShown[indexClick] == false) {
+      allListingDetails[indexClick].style.display = "";
+      detailsShown[indexClick] = true;
+    } else {
+      allListingDetails[indexClick].style.display = "none";
+      detailsShown[indexClick] = false;
+    }
+  });
+});
+
+
+
+
+});
