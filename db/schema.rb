7742029f027c635af5a50d66f43f816aa6fb7ef5
Felipe Santos <santos.felipe13@gmail.com>
2018-11-07 20:27:57 -0200

    added progress to user and seed basic ship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_205923) do
+ActiveRecord::Schema.define(version: 2018_11_07_221917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2018_11_07_205923) do
     t.string "username"
     t.integer "coins"
     t.integer "score"
+    t.integer "progress", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
